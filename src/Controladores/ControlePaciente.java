47fcbd96d1aea9a627daf0abb5dd1494fbf2d0b7
mateushtoledo <mateushtoledo@gmail.com>
2016-06-30 14:24:10 -0300

    Atualização
    Atualização da classe ControlePaciente

--- src/Controladores/ControlePaciente.java
+++ src/Controladores/ControlePaciente.java
@@ -6,7 +6,6 @@ import Entidades.*;
 import Limites.*;
 
 public class ControlePaciente {
-
     private ArrayList<Paciente> objPacientes;
     private ArrayList<Paciente> objFalecidos;
 
@@ -14,118 +13,129 @@ public class ControlePaciente {
         objPacientes = new ArrayList<>();
         objFalecidos = new ArrayList<>();
     }
-
-    public void cadastrarPaciente(String pNome, String pNumBeneficiaro, String pSexo, String pEndereco, String pTelefone, Date pNascimento) {
+    
+    //Metodo que adiciona paciente ao sistema
+    public void cadastrarPaciente(String pNome,String pNumBeneficiaro,String pSexo,String pEndereco,String pTelefone,Date pNascimento)
+    {
         objPacientes.add(new Paciente(pNome, pNumBeneficiaro, pSexo, pEndereco, pTelefone, pNascimento));
     }
-
-    public void cadastrarFalecimentoNumBene(String pNumBeneficiario) {
-        for (Paciente pac : objPacientes) {
-            if (pac.getNumBeneficiario().equals(pNumBeneficiario)) {
-                objFalecidos.add(pac);
-                objPacientes.remove(pac);
+    
+    //Metodo que cadastra o falecimento de determinado paciente.
+    //O falecimento nao implica na exclusao dos dados, mas sim em salva-los
+    //separados dos demais.
+    public void cadastrarFalecimento(String pNumBeneficiario)
+    {
+        for(int i=0 ; i<objPacientes.size() ; i++)
+        {
+            if(objPacientes.get(i).getNumBeneficiario().equalsIgnoreCase(pNumBeneficiario))
+            {
+                objFalecidos.add(objPacientes.get(i));
+                objPacientes.remove(i);
             }
         }
     }
-
-    public void cadastrarFalecimentoNome(String pNome) {
-        for (Paciente pac : objPacientes) {
-            if (pac.getNome().equals(pNome)) {
-                objFalecidos.add(pac);
-                objPacientes.remove(pac);
-            }
-        }
-    }
-
-    public void interfaceCadastroPaciente() {
-        new LimiteCadastroPaciente(this);
-    }
-
-    public void alterarEndereco(Paciente pac, String pEndereco) {
+    
+    //Metodo que realiza a alteracao do endereco do paciente
+    public void alterarEndereco(Paciente pac,String pEndereco)
+    {
         pac.setEndereco(pEndereco);
     }
-
-    public void alterarTelefone(Paciente pac, String pTelefone) {
+    
+    //Metodo que realiza a alteracao do telefone do paciente
+    public void alterarTelefone(Paciente pac,String pTelefone)
+    {
         pac.setTelefone(pTelefone);
     }
-
-    public ArrayList<Paciente> getListaPacientes() {
+    
+    //Metodo que retorna a lista de pacientes salva no sistema
+    public ArrayList<Paciente> getListaPacientes()
+    {
         return objPacientes;
     }
-
-    public Object[] getDescricaoPacientes() {
+    
+    //Metodo que retorna uma descricao dos pacientes contidos no sistema em 
+    //Vetor de objetos.
+    //Utilizado para criar objetos do tipo combo Box nas interfaces
+    public Object[] getDescricaoPacientes()
+    {
         Object itens[] = new Object[10000];
-        int i = 0;
-
-        for (Paciente pac : objPacientes) {
-            String p = pac.getNumBeneficiario() + " - " + pac.getNome();
+        int i=0;
+        
+        for(Paciente pac : objPacientes)
+        {
+            String p = pac.getNumBeneficiario()+" - "+pac.getNome();
             itens[i] = p;
             i++;
         }
-
+        
         Object desc[] = new Object[i];
-
+        
         System.arraycopy(itens, 0, desc, 0, i);
-
+        
         return desc;
     }
-
-    public Paciente getPaciente(String pNumBeneficiario) {
-        for (Paciente pac : objPacientes) {
-            if (pac.getNumBeneficiario().equals(pNumBeneficiario)) {
+    
+    //Retorna o paciente que contem o numero de beneficiario passado (ou nulo)
+    public Paciente getPaciente(String pNumBeneficiario)
+    {
+        for(Paciente pac : objPacientes)
+        {
+            if(pac.getNumBeneficiario().equals(pNumBeneficiario))
                 return pac;
-            }
         }
-
+        
         return null;
     }
-
-    public boolean adicionarProntuario(String pNumBeneficiario, String pQueuixas, String pResumoExame, String pResumoDiagnostico, String pTratamentos, Date pData) {
-        for (Paciente pac : objPacientes) {
-            if (pac.getNumBeneficiario().equals(pNumBeneficiario)) {
-                pac.getDadosMedicos().add(new Prontuario(pNumBeneficiario, pQueuixas, pResumoExame, pResumoDiagnostico, pTratamentos, pData));
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void interfaceAlteracaoDadosContato() {
+    
+    //Metodo que exibe interface para alteracao dos dados de contato do paciente
+    public void interfaceAlteracaoDadosContato()
+    {
         new LimiteAltDadosPaciente(this);
     }
-
-    public void salvarPacientes() throws Exception {
+    
+    //Metodo que exibe interface para realizar cadastro de paciente
+    public void interfaceCadastroPaciente()
+    {
+        new LimiteCadastroPaciente(this);
+    }
+    
+    //Metodo que salva a lista de pacientes em arquivo de texto
+    public void salvarPacientes() throws Exception
+    {
         FileOutputStream fileOs = new FileOutputStream("pacientes.dat");
         ObjectOutputStream objOs = new ObjectOutputStream(fileOs);
         objOs.writeObject(objPacientes);
-
+        
         fileOs = new FileOutputStream("pacientesfalecidos.dat");
         objOs = new ObjectOutputStream(fileOs);
         objOs.writeObject(objFalecidos);
-
+        
         objOs.flush();
         objOs.close();
     }
-
-    public void recuperarPacientes() throws Exception {
+    
+    //Metodo que recupera a lista de pacientes do sistema salva em arquivo de texto
+    public void recuperarPacientes() throws Exception
+    {
         File arquivo = new File("pacientes.dat");
-
+        
         //Recuperar Dados dos pacientes ativos no sistema
-        if (arquivo.exists()) {
+        if(arquivo.exists())
+        {
             FileInputStream fileIs = new FileInputStream(arquivo);
             ObjectInputStream objIs = new ObjectInputStream(fileIs);
-            objPacientes = (ArrayList<Paciente>) objIs.readObject();
+            objPacientes = (ArrayList<Paciente>)objIs.readObject();
             objIs.close();
         }
-
+        
         //Recuperar dados dos pacientes falecidos
         arquivo = new File("pacientesfalecidos.dat");
-        if (arquivo.exists()) {
+        if(arquivo.exists())
+        {
             FileInputStream fileIs = new FileInputStream(arquivo);
             ObjectInputStream objIs = new ObjectInputStream(fileIs);
-            objFalecidos = (ArrayList<Paciente>) objIs.readObject();
+            objFalecidos = (ArrayList<Paciente>)objIs.readObject();
             objIs.close();
         }
     }
-
 }
